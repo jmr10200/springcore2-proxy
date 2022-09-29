575169098a92922eec1b7b9e3932a5a13574e015
jmr <jmr10200@gmail.com>
2022-09-29 20:32:55 +0900

    예제 프로젝트 V2

--- src/main/java/hello/proxy/config/AppV1Config.java
+++ src/main/java/hello/proxy/config/AppV1Config.java
@@ -8,17 +8,17 @@ import org.springframework.context.annotation.Configuration;
 public class AppV1Config {
 
     @Bean
-    public OrderControllerV1 orderController() {
-        return new OrderControllerV1Impl(orderService());
+    public OrderControllerV1 orderControllerV1() {
+        return new OrderControllerV1Impl(orderServiceV1());
     }
 
     @Bean
-    public OrderServiceV1 orderService() {
-        return new OrderServiceV1Impl(orderRepository());
+    public OrderServiceV1 orderServiceV1() {
+        return new OrderServiceV1Impl(orderRepositoryV1());
     }
 
     @Bean
-    public OrderRepositoryV1 orderRepository() {
+    public OrderRepositoryV1 orderRepositoryV1() {
         return new OrderRepositoryV1Impl();
     }
 }
